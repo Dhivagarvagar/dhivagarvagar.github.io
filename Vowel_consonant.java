650c19f88923c4e535ca139c4de187fd8c6326dc
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2018-09-04 17:50:03 +0530

    Update Vowel_consonant.java

--- Vowel_consonant.java
+++ Vowel_consonant.java
@@ -9,8 +9,11 @@ public class Vowel_consonant {
 		if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E'  || ch=='I' || ch=='O' || ch=='U') {
 			System.out.println("Vowels");
 		}
-		else {
+		else if ((ch>='a' && ch<='z') || (ch>='A' && ch>='Z')) {
 			System.out.println("Consonant");
 		}	
+		else {
+			System.out.println("Invalid");
+		}
 	}
 }
