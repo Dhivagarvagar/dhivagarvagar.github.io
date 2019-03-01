edf333c8e5b5df740b65861b57738e87f709a166
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-03-01 17:31:15 +0530

    Add files via upload

---
+++ Natural_nos.java
@@ -0,0 +1,13 @@
+package clas;
+import java.util.*;
+public class Natural_nos {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int sum=0;
+	for(int i=0;i<=n;i++) {
+		sum+=i;
+		}
+	System.out.print(sum);
+}
+}
