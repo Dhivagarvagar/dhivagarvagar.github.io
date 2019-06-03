11c55b8bc4324884eda9b29f38c5465ddf25f871
Dhivagarvagar <31370701+Dhivagarvagar@users.noreply.github.com>
2019-06-03 13:43:43 +0530

    Add files via upload

---
+++ Odd_limit.java
@@ -0,0 +1,14 @@
+package clas;
+import java.util.*;
+public class Odd_limit {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	for(int i=a+1;i<b;i++) {
+		if(i%2!=0) {
+			System.out.print(i+" ");
+		}
+	}
+}
+}
